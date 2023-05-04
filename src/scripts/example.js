bcaa4eed37d1a7368321384eec74532f101b3e9e
Naoko Sugawara <infoseek122@gmail.com>
2023-05-04 18:56:16 -0400

    first commit

---
+++ src/scripts/example.js
@@ -0,0 +1,10 @@
+class Example {
+    constructor(htmlElement){
+        this.htmlElement = htmlElement
+        this.htmlElement.innerHTML = "<h1>bello!!</h1>"
+    }
+
+    handleClick(){
+        this.htmlElement.children[0].i
+    }
+}
